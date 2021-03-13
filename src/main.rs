7ab3e7d1ae0710e965c6ad43b72ff16cc433df0c
Uwe Pachler <uwe.pachler@gmail.com>
2021-03-13 16:14:52 +0100

    add reqwest to fetch JWK from auth server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use alcoholic_jwt::{JWK, JWKS};
+use serde::Deserialize;
+use std::fmt;
 
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, post, web};
-use juniper::{EmptySubscription, EmptyMutation};
+use juniper::{EmptyMutation, EmptySubscription};
 use juniper_actix::{graphiql_handler, graphql_handler, playground_handler};
 use web::Payload;
 
-use graphql_schema::{Context, Schema, Query};
+use graphql_schema::{Context, Query, Schema};
 
 mod graphql_schema;
 
 const GRAPHQL_PATH: &str = "/graphql";
 
+#[derive(Deserialize)]
+struct OidcConfig {
+    jwks_uri: String,
+}
+
+#[derive(fmt::Debug)]
+enum Error {
+    CannotFindAuthorizationSigningKey(String),
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::CannotFindAuthorizationSigningKey(kid) => {
+                write!(f, "No key with KID {} was found", kid)
+            }
+        }
+    }
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -17,14 +41,22 @@ async fn hello() -> impl Responder {
 }
 
 #[get("/graphql")]
-async fn handle_graphql_get(req: HttpRequest, payload: Payload, schema: web::Data<Schema>) -> impl Responder {
-    let context = Context{};
+async fn handle_graphql_get(
+    req: HttpRequest,
+    payload: Payload,
+    schema: web::Data<Schema>,
+) -> impl Responder {
+    let context = Context {};
     graphql_handler(&schema, &context, req, payload).await
 }
 
 #[post("/graphql")]
-async fn handle_graphql_post(req: HttpRequest, payload: Payload, schema: web::Data<Schema>) -> impl Responder {
-    let context = Context{};
+async fn handle_graphql_post(
+    req: HttpRequest,
+    payload: Payload,
+    schema: web::Data<Schema>,
+) -> impl Responder {
+    let context = Context {};
     graphql_handler(&schema, &context, req, payload).await
 }
 
@@ -38,25 +70,57 @@ async fn handle_playground() -> impl Responder {
     playground_handler(GRAPHQL_PATH, None).await
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    let addrs = ["127.0.0.1:8080"];
+async fn fetch_jwks(uri: &str) -> Result<JWKS, Box<dyn std::error::Error>> {
+    let res = reqwest::get(uri).await?;
+    let jwks = res.json::<JWKS>().await?;
+    Ok(jwks)
+}
 
+async fn provide_jwk(kid: &str) -> Result<JWK, Box<dyn std::error::Error>> {
+    let discovery_uri = "http://localhost:8080/auth/realms/test/";
+    let oidc_config_uri = String::from(discovery_uri) + ".well-known/openid-configuration";
+    let oidc_config = reqwest::get(oidc_config_uri)
+        .await?
+        .json::<OidcConfig>()
+        .await?;
 
-    let mut srv = HttpServer::new(|| {
-        App::new()
-        .data(Schema::new(Query, EmptyMutation::<Context>::new(), EmptySubscription::<Context>::new()))
-        .service(handle_graphql_get)
-        .service(handle_graphql_post)
-        .service(handle_graphiql)
-        .service(handle_playground)
-        .service(hello)
-    });
+    let jwks = fetch_jwks(&oidc_config.jwks_uri).await?;
+    println!("jwks: {:?}", jwks);
 
-    
-    for addr in addrs.iter() {
-        srv = srv.bind(addr)?;
+    jwks.find(kid)
+        .map(|jwk| jwk.clone())
+        .ok_or_else(|| Error::CannotFindAuthorizationSigningKey(kid.into()).into())
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let addrs = ["127.0.0.1:8081"];
+
+    // only for testing right now
+    if let Err(e) = provide_jwk("foo").await {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            e.to_string(),
+        ));
     }
 
-    srv.run().await
+    let mut actix_srv = HttpServer::new(|| {
+        App::new()
+            .data(Schema::new(
+                Query,
+                EmptyMutation::<Context>::new(),
+                EmptySubscription::<Context>::new(),
+            ))
+            .service(handle_graphql_get)
+            .service(handle_graphql_post)
+            .service(handle_graphiql)
+            .service(handle_playground)
+            .service(hello)
+    });
+
+    for addr in addrs.iter() {
+        actix_srv = actix_srv.bind(addr)?;
+    }
+
+    actix_srv.run().await
 }
