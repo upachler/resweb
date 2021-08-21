5c6a96788f53748bd06a8cf4ea18b9ae2c50d9cb
Pachler, Uwe <uwe.pachler@datagroup.de>
2021-08-21 19:12:50 +0200

    replace icon

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -3,8 +3,12 @@ use std::collections::HashMap;
 const DASHBOARD_HTML_FILENAME: &'static str = "dashboard.html";
 const DASHBOARD_HTML_CONTENT: &[u8] = std::include_bytes!("dashboard.html");
 
+const FAVICON_FILENAME: &'static str = "favicon-32x32.png";
+const FAVICON_CONTENT: &[u8] = std::include_bytes!("favicon-32x32.png");
+
 pub fn resources() -> HashMap<&'static str, &'static [u8]> {
     let mut m = HashMap::new();
     m.insert(DASHBOARD_HTML_FILENAME, DASHBOARD_HTML_CONTENT);
+    m.insert(FAVICON_FILENAME, FAVICON_CONTENT);
     m
 }
