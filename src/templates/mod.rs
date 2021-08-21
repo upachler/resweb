b2932288bd94df6bc83a6f58cf97715018cb626a
Uwe Pachler <uwe.pachler@gmail.com>
2021-08-21 19:21:52 +0200

    move stylesheet to its own file

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -6,9 +6,13 @@ const DASHBOARD_HTML_CONTENT: &[u8] = std::include_bytes!("dashboard.html");
 const FAVICON_FILENAME: &'static str = "favicon-32x32.png";
 const FAVICON_CONTENT: &[u8] = std::include_bytes!("favicon-32x32.png");
 
+const STYLE_CSS_FILENAME: &'static str = "style.css";
+const STYLE_CSS_CONTENT: &[u8] = std::include_bytes!("style.css");
+
 pub fn resources() -> HashMap<&'static str, &'static [u8]> {
     let mut m = HashMap::new();
     m.insert(DASHBOARD_HTML_FILENAME, DASHBOARD_HTML_CONTENT);
     m.insert(FAVICON_FILENAME, FAVICON_CONTENT);
+    m.insert(STYLE_CSS_FILENAME, STYLE_CSS_CONTENT);
     m
 }
